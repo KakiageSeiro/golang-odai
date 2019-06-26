e65d21056282ce8ad02cf83f7e46d9081a718147
YutaEbisuno <frederika07_bernkastel@yahoo.co.jp>
2019-06-26 09:34:50 +0900

    Gormを導入

--- db/db.go
+++ db/db.go
@@ -1,38 +1,57 @@
 package db
 
 import (
-	"database/sql"
+	//"database/sql" //標準ライブラリ利用
+	//_ "github.com/go-sql-driver/mysql"
+
 	_ "github.com/go-sql-driver/mysql"
+	//Gorm利用
+	"github.com/jinzhu/gorm"
 	"time"
 )
 
 //接続先
 //Docker-composeでのmy-golang-odai_db_1のように、dbという文字列が存在していれば以下のようにtcp(db)でOK
-const dsn = "root@tcp(db)/twitter"
+const DSN = "root@tcp(db)/twitter"
 
 // DB database interface
 type DB struct {
-	conn *sql.DB
+	//conn *sql.DB
+	conn *gorm.DB
 }
 
 //DB接続し、コネクションを取得
 func GetConnection() (*DB, error) {
+
 	//DB接続
-	conn, err := sql.Open("mysql", dsn)
+	//if dsn == ""{ //テスト環境で接続先を変えようとした残滓
+	//	dsn = DSN
+	//}
+
+	//標準ライブラリ利用
+	//conn, err := sql.Open("mysql", DSN)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//db := DB{conn: conn}
+
+	//Gorm利用
+	conn, err := gorm.Open("mysql", DSN)
 	if err != nil {
-		return nil, err
+		panic("DBに接続できませんでした。。。■" + err.Error())
 	}
+	//defer conn.Close()
 	db := DB{conn: conn}
 
 	//コネクションの最大ライフタイム（タイムアウト時間）
-	conn.SetConnMaxLifetime(10 * time.Second)
+	conn.DB().SetConnMaxLifetime(10 * time.Second)
 	//コネクションの最大コネクション数
-	conn.SetMaxOpenConns(10)
+	conn.DB().SetMaxOpenConns(10)
 	//コネクションの最大待機数
-	conn.SetMaxIdleConns(10)
+	conn.DB().SetMaxIdleConns(10)
 
 	//接続確認
-	if err := conn.Ping(); err != nil {
+	if err := conn.DB().Ping(); err != nil {
 		return nil, err
 	}
 
@@ -40,6 +59,6 @@ func GetConnection() (*DB, error) {
 }
 
 // Open returns the database connection.
-func (d *DB) Open() *sql.DB {
+func (d *DB) Open() *gorm.DB {
 	return d.conn
 }
--- model/posts.go
+++ model/posts.go
@@ -2,10 +2,10 @@ package model
 
 import (
 	"context"
-	"log"
 	"my-golang-odai/db"
-	"strconv"
-	"github.com/jinzhu/gorm"
+
+	//Gorm利用
+	_ "github.com/jinzhu/gorm"
 )
 
 //ツイートのデータ型
@@ -24,21 +24,27 @@ func RetrieveLists(ctx context.Context) ([]Post, error) {
 	}
 
 	//すべての投稿を取得
-	rows, e := db.Open().QueryContext(
-		ctx,
-		"select name, text from posts",
-	)
 
-	//リストにして返却
+	//標準ライブラリ利用
+	//rows, e := db.Open().QueryContext(
+	//	ctx,
+	//	"select name, text from posts",
+	//)
+	//
+	////リストにして返却
+	//list := make([]Post, 0)
+	//for rows.Next() {
+	//	//１レコード取り出してリストに追加
+	//	var p Post
+	//	if err := rows.Scan(&p.Name, &p.Text); err != nil {
+	//		return nil, err
+	//	}
+	//	list = append(list, p)
+	//}
+
+	//Gorm利用
 	list := make([]Post, 0)
-	for rows.Next() {
-		//１レコード取り出してリストに追加
-		var p Post
-		if err := rows.Scan(&p.Name, &p.Text); err != nil {
-			return nil, err
-		}
-		list = append(list, p)
-	}
+	db.Open().Find(&list)
 
 	return list, nil
 }
@@ -52,32 +58,36 @@ func RetrieveOneTweet(ctx context.Context, id int) (*Post, error) {
 	}
 
 	//投稿を取得
-	rows, e := db.Open().QueryContext(
-		ctx,
-		"select name, text from posts where id = ?",
-		id,
-		//strconv.Itoa(id),
-	)
-	if e != nil {
-		return nil, e
-	}
-
-	//リストにして返却
-	list := make([]Post, 0)
-	var p Post
-	for rows.Next() {
-		//１レコード取り出してリストに追加
-		if err := rows.Scan(&p.Name, &p.Text); err != nil {
-			return nil, err
-		}
-		list = append(list, p)
-	}
-	log.Printf("■ツイート一見取得処理で取得できたレコード数:" + strconv.Itoa(len(list)))
+	//rows, e := db.Open().QueryContext(
+	//	ctx,
+	//	"select name, text from posts where id = ?",
+	//	id,
+	//	//strconv.Itoa(id),
+	//)
+	//if e != nil {
+	//	return nil, e
+	//}
+	//
+	////リストにして返却
+	//list := make([]Post, 0)
+	//var p Post
+	//for rows.Next() {
+	//	//１レコード取り出してリストに追加
+	//	if err := rows.Scan(&p.Name, &p.Text); err != nil {
+	//		return nil, err
+	//	}
+	//	list = append(list, p)
+	//}
+	//log.Printf("■ツイート一見取得処理で取得できたレコード数:" + strconv.Itoa(len(list)))
 
 	//TODO:１レコードであることをチェックして、エラーの場合↓
 	//NotFoundエラーを作って返す。呼び出し側でエラー種別をcaseしてハンドリングする
 
-	return &p, nil
+	//Gorm利用
+	post := &Post{}
+	db.Open().Where("id = ?", id).First(&post) //ポインタのポインタを渡すことに注意
+
+	return post, nil
 }
 
 //ツイートする
@@ -89,14 +99,20 @@ func Tweet(ctx context.Context, name string, text string) error {
 	}
 
 	//投稿内容を保存
-	if _, e := db.Open().QueryContext(
-		ctx,
-		"insert into posts(name, text) value(?, ?)",
-		name,
-		text,
-	); e != nil {
-		//失敗した場合
-		return e
+	//if _, e := db.Open().QueryContext(
+	//	ctx,
+	//	"insert into posts(name, text) value(?, ?)",
+	//	name,
+	//	text,
+	//); e != nil {
+	//	//失敗した場合
+	//	return e
+	//}
+
+	post := &Post{Name:name, Text:text}
+	if result := db.Open().NewRecord(&post); result == true{
+		db.Open().Create(&post)
 	}
+
 	return nil
 }

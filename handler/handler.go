ebd6a3ce4c2e6fac253ee1838830ab145ebf05a3
YutaEbisuno <frederika07_bernkastel@yahoo.co.jp>
2019-06-26 21:47:04 +0900

    renderを導入

--- handler/handler.go
+++ handler/handler.go
@@ -2,26 +2,38 @@ package handler
 
 import (
 	"github.com/go-chi/chi"
-	"html/template"
+	"github.com/unrolled/render"
 	"my-golang-odai/model"
 	"net/http"
 	"strconv"
 )
 
+//ツイート一覧
+type Data struct{
+	List []model.Post
+}
+
 //ツイート一覧画面
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	//ツイートをすべて取得
-	lists, err := model.RetrieveLists(r.Context())
+	list, err := model.RetrieveLists(r.Context())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	// テンプレート読み込み
-	t := template.Must(template.ParseFiles("template/Index.html"))
-	// テンプレートを描画
-	if err := t.Execute(w, lists); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	//標準ライブラリ利用
+	//// テンプレート読み込み
+	//t := templates.Must(templates.ParseFiles("templates/Index.html"))
+	//// テンプレートを描画
+	//if err := t.Execute(w, list); err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//}
+
+	//render利用
+	data := Data{
+		list,
 	}
+	renderer(w, "index", data)
 }
 
 //単一のツイート表示画面
@@ -29,7 +41,7 @@ func OneTweetHandler(w http.ResponseWriter, r *http.Request) {
 	//クエリパラメータ取得
 	id := chi.URLParam(r, "id")
 	if id == "" {
-		//TODO:新しくエラーを作って返したいけどよくわからん
+		//TODO:新しくエラーを作って返したいね
 		//http.Error(w, errors.New(""), http.StatusInternalServerError)
 	}
 
@@ -44,22 +56,31 @@ func OneTweetHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
+	//標準ライブラリ利用
 	// テンプレート読み込み
-	t := template.Must(template.ParseFiles("template/tweet.html"))
-	// テンプレートを描画
-	if err := t.Execute(w, *post); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	//t := template.Must(template.ParseFiles("templates/tweet.html"))
+	//// テンプレートを描画
+	//if err := t.Execute(w, *post); err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//}
+
+	//render利用
+	renderer(w, "tweet", post)
 }
 
 //投稿（ツイート）用フォーム画面
 func FormHandler(w http.ResponseWriter, r *http.Request) {
-	// テンプレート読み込み
-	t := template.Must(template.ParseFiles("template/form.html"))
-	// テンプレートを描画
-	if err := t.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+
+	//標準ライブラリ利用
+	//// テンプレート読み込み
+	//t := template.Must(template.ParseFiles("templates/form.html"))
+	//// テンプレートを描画
+	//if err := t.Execute(w, nil); err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//}
+
+	//render利用
+	renderer(w, "form", nil)
 }
 
 //投稿（ツイート）する
@@ -81,4 +102,21 @@ func TweetHandler(w http.ResponseWriter, r *http.Request) {
 
 	//リダイレクト(index行き)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+	//TODO:この方法だとHTTPヘッダに余計な書き込みをしているメッセージが出る（HTMLレンダリング自体はできてる）
+	//render利用
+	//re := render.New(render.Options{
+	//	Extensions: []string{".html"},
+	//	Charset:    "UTF-8",
+	//})
+	//re.HTML(w, http.StatusSeeOther, "index", nil)
+}
+
+//render利用でHTMLレンダリングする
+func renderer(w http.ResponseWriter, templateName string, data interface{}) {
+	re := render.New(render.Options{
+		Extensions: []string{".html"},
+		Charset:    "UTF-8",
+	})
+	re.HTML(w, http.StatusOK, templateName, data)
 }
